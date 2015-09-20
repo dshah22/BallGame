f99bbd07e3dc7c9c6621514ffcbc398b9dd01769
dshah22 <dshah22@terpmail.umd.edu>
2015-09-20 15:09:22 -0400

    Init commit

---
+++ build/aplite/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "control",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = 0,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x74, 0x9D, 0x7F, 0xCF, 0x07, 0x31, 0x49, 0x2E, 0xA6, 0x6C, 0x80, 0xC6, 0x60, 0xED, 0xE9, 0x69 },
+  .virtual_size = 0xb6b6
+};
